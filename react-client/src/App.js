1bbd8e9265f609cbc2e1941852f65289937e9f01
egChoi <2013agal@gmail.com>
2024-09-05 23:41:47 +0900

    4-1. React와 Apollo Client
    4. GraphQL 클라이언트 만들어보기

---
+++ react-client/src/App.js
@@ -0,0 +1,55 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import React, { useState } from "react";
+import "./App.css";
+
+import People from "./components/people";
+import Roles from "./components/roles";
+import Teams from "./components/teams";
+
+// client: GraphQL 서버로와 정보를 주고받을 ApolloClient 객체
+const client = new ApolloClient({
+  uri: "http://localhost:4000", // uri: GraphQL 서버의 주소
+  cache: new InMemoryCache(), // cache: InMemoryCache를 통한 캐시 관리
+});
+
+function App() {
+  const [menu, setMenu] = useState("Roles");
+
+  let mainComp = {
+    Roles: <Roles />,
+    Teams: <Teams />,
+    People: <People />,
+  };
+
+  function NavMenus() {
+    return ["Roles", "Teams", "People"].map((_menu, key) => {
+      return (
+        <li
+          key={key}
+          className={menu === _menu ? "on" : ""}
+          onClick={() => {
+            setMenu(_menu);
+          }}
+        >
+          {_menu}
+        </li>
+      );
+    });
+  }
+
+  return (
+    <div className="App">
+      <ApolloProvider client={client}>
+        <header className="App-header">
+          <h1>Company Management</h1>
+          <nav>
+            <ul>{NavMenus()}</ul>
+          </nav>
+        </header>
+        <main>{mainComp[menu]}</main>
+      </ApolloProvider>
+    </div>
+  );
+}
+
+export default App;
